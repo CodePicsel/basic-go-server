64c8b5f7ea8aaecabe81b6cfd5210982639a6eef
CodePicsel <bankarshashank9@gmail.commkdir>
2025-02-23 19:07:22 +0530

    Route Handlers Added

--- main.go
+++ main.go
@@ -6,14 +6,38 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request){
+   if err := r.ParseForm(); err != nil {
+       fmt.Fprintf(w, "ParseForm() err: %v", err)
+       return
+   }
+   fmt.Fprintf(w, " POST request Successfull \n")
+   name := r.FormValue("name")
+   address := r.FormValue("address")
+   fmt.Fprintf(w, "Name: %s", name)
+   fmt.Fprintf(w, "Address: %s", address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+   if r.URL.Path != "/hello"{
+      http.Error(w, "404 Page Not Found!", http.StatusNotFound)
+      return
+   }
+   if r.Method != "GET" {
+      http.Error(w, "Wrong Method Set", http.StatusNotFound)
+      return
+   }
+   fmt.Fprintf(w, "Hello World!")
+}
+
 func main(){
    fileServer := http.FileServer(http.Dir("./static"))
    http.Handle("/", fileServer)
    http.HandleFunc("/form", formHandler)
-   http.HandlerFunc("/hello", helloHandler)
+   http.HandleFunc("/hello", helloHandler)
 
    fmt.Println("Starting the server...")
-   if err := http.ListenAndServe(":8080", nil) err != nil {
+   if err := http.ListenAndServe(":8080", nil); err != nil {
 	log.Fatal(err)
    }
 
